e9bedb08552e5e04c84d100cee33b6ff46a39199
isaac_thinkpad <isaac@thinkpad>
2019-11-06 21:35:19 -0800

    Fixed all errors and built

--- EmailConfirmationServer/App_Start/RouteConfig.cs
+++ EmailConfirmationServer/App_Start/RouteConfig.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using Microsoft.AspNetCore.Mvc
+using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace EmailConfirmationServer
--- EmailConfirmationServerCore/Data/ApplicationDbContext.cs
+++ EmailConfirmationServerCore/Data/ApplicationDbContext.cs
@@ -1,16 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using EmailConfirmationServer.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace EmailConfirmationServerCore.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext, IEmailConfirmationContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        IQueryable<Person> IEmailConfirmationContext.People
+        {
+            get { return People; }
+        }
+
+        public DbSet<Person> People { get; set; }
+
+        IQueryable<Email> IEmailConfirmationContext.Emails
+        {
+            get { return Emails; }
+        }
+
+        public DbSet<Email> Emails { get; set; }
+
+        Person IEmailConfirmationContext.FindPersonById(int id)
+        {
+            return Set<Person>().Find(id);
+        }
+
+        IQueryable<SheetUpload> IEmailConfirmationContext.Uploads
+        {
+            get { return Uploads; }
+        }
+
+        public DbSet<SheetUpload> Uploads { get; set; }
+
+        IQueryable<User> IEmailConfirmationContext.Users
+        {
+            get { return Users; }
+        }
+
+        public DbSet<User> Users { get; set; }
+
+
+        void IEmailConfirmationContext.SaveChanges()
+        {
+            SaveChanges();
+        }
+
+        void IEmailConfirmationContext.Add<T>(T entity)
+        {
+            Add<T>(entity);
+        }
+
+        IQueryable<Email> IEmailConfirmationContext.FindEmailById(int id)
+        {
+            var emails = from email in Emails
+                         where email.Id == id
+                         select email;
+            return emails;
+        }
+
+        User IEmailConfirmationContext.FindUserById(string id)
+        {
+            var user = Users
+                .Where(u => u.Id == id)
+                .Include(u => u.Uploads)
+                    .ThenInclude(s => s.People)
+                    .ThenInclude(p => p.Emails)
+                .FirstOrDefault();
+
+            return user;
+        }
     }
 }
+
