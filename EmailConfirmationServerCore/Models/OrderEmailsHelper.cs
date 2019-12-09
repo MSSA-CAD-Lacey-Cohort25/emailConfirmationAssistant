72534c4128a602a0f35c763e1340450cedb2c8cb
isaacaflores2 <isaac.a.flores2@gmail.com>
2019-12-09 12:39:25 -0800

    Added helper function to order emails

---
+++ EmailConfirmationServerCore/Models/OrderEmailsHelper.cs
@@ -0,0 +1,30 @@
+﻿using EmailConfirmationServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmailConfirmationServerCore.Models
+{
+    public class OrderEmailsHelper
+    {
+        public static List<Email> OutlookFirst(List<Email> emails)
+        {
+            Email[] orderedEmails = new Email[2];
+
+            foreach(var email in emails)
+            {
+                if (email.EmailAddress.Contains("outlook"))
+                {
+                    orderedEmails[0] = email;
+                }
+                else
+                {
+                    orderedEmails[1] = email;
+                }
+            }
+
+            return orderedEmails.ToList();
+        }
+    }
+}
